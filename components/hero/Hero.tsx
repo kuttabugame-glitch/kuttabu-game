3f4104d76e0253b82959a767f9e1a89b699b183d
kuttabugame-glitch <kuttabugame@gmail.com>
2026-07-11 13:57:45 +0300

    Replace store buttons with coming soon badge

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -8,7 +8,14 @@ export default function Hero() {
       <div className="absolute left-1/2 top-1/2 z-10 h-[780px] w-[920px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#F7F3EA]/80 blur-[70px]" />
       <div className="absolute left-1/2 top-1/2 z-10 h-[520px] w-[720px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#B28A3C]/10 blur-[90px]" />
 
-     
+      <div className="relative z-20 mx-auto flex max-w-6xl animate-hero-in flex-col items-center text-center">
+        <img
+          src="/images/logo.svg"
+          alt="Kuttabu"
+          draggable={false}
+          className="h-auto w-[min(92vw,780px)] select-none"
+        />
+      </div>
     </section>
   );
 }
